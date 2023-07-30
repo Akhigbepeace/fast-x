1195409293b029ef4ce843b18d99600aecd265e3
Akhigbepeace <akhigbepeace2005@gmail.com>
2023-07-30 22:08:52 +0100

    adds side ba

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Blog from './pages/Blog'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Sidebar from './component/molecules/Sidebar'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/loggedIn" element={<Sidebar />} />
       </Routes>
     </Router>
   )
---
+++ src/component/layouts/OrderPickup.jsx
@@ -0,0 +1,8 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const OrderPickup = () => {
+  return <Box>OrderPickup</Box>
+}
+
+export default OrderPickup
